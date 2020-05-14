65ceb21c897d93957a78342ff1887dd18b1eb904
Htocki <pasztownia@gmail.com>
2020-05-14 16:28:30 +0300

    Сохранение.

--- L6/gchmod.bash
+++ L6/gchmod.bash
@@ -10,3 +10,39 @@
 # -- каталог, в котором нужно производить поиск;
 # -- права для поиска; 
 # -- целевые права.
+
+parameter_count=$#
+if [[ $parameter_count = 3 ]]; then
+  directory=$1
+  if [[ -e $directory ]]; then
+    echo "OK"
+  else 
+    echo "Ошибка: Указанная директория не существует.">&2
+    exit
+  fi
+
+  permissions='^[0-7]+$'
+  find_permissions=$2
+  if [[ $find_permissions =~ $permissions ]]; then
+  echo "OK"
+  else 
+    echo "Ошибка: Неверно указаны права для поиска.">&2
+    exit
+  fi
+  
+  target_permissions=$3
+  if [[ $target_permissions =~ $permissions ]]; then
+  echo "OK"
+  else
+    echo "Ошибка: Неверно указаны целевые права.">&2
+    exit
+  fi
+
+  files=$(find -type f -perm $find_permissions)
+  chmod $target_permissions $files
+  
+  fi
+else
+  echo "Ошибка: Неверное число параметров." >&2
+  exit
+fi
